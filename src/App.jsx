1c33b0d153508bf503936769bfa8e6d2fa78ca1f
Marcus Pousette <marcuspousette@gmail.com>
2023-02-23 14:09:18 +0100

    changes for lesson 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
 import './App.css'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom'
+
+// pages
+import Home from './pages/Home'
+import About from './pages/About'
+
+// layouts
+import RootLayout from './layouts/RootLayout'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+      <Route path="/" element={<RootLayout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+      </Route>
+  )
+)
 
 function App() {
-    return <div className="App">Hello Techover</div>
+  return <RouterProvider router={router} />
 }
 
 export default App
---
+++ src/layouts/RootLayout.jsx
@@ -0,0 +1,21 @@
+import { Outlet, NavLink } from 'react-router-dom'
+import logo from '../assets/logo.png'
+
+export default function RootLayout() {
+    return (
+        <div className="root-layout">
+            <header>
+                <nav className="navbar">
+                    <img className="logo" src={logo} alt="Techover" />
+                    <div className="nav-links">
+                        <NavLink className="nav-link" to="/">Hem</NavLink>
+                        <NavLink className="nav-link" to="about">Om oss</NavLink>
+                    </div>
+                </nav>
+            </header>
+            <main>
+                <Outlet />
+            </main>
+        </div>
+    )
+}
